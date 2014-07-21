7790b791fbd495e61d5300e9960314605272c024
Tomdanizer <tomdanizer@gmail.com>
2014-07-20 19:33:39 -0500

    nodejs uploading modules installed & basic uploading/file validation configured.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../mysql.js');
+var fs = require('fs');
+var busboy = require('connect-busboy');
 
 /*
  *  Get index page page initial query request of all train schedules
@@ -20,4 +22,35 @@ router.get('/', function(req, res) {
     });
 });
 
+router.post('/upload', function(req, res,next){
+var fstream, fileExtension;
+        req.pipe(req.busboy);
+        req.busboy.on('file', function (fieldname, file, filename,  encoding, mimetype) {
+            console.log("Uploading: " + filename);
+            fileExtension = filename.split('.')[filename.split('.').length - 1].toLowerCase();
+            mimetype = mimetype.toLowerCase();
+            if(mimetype === 'application/csv' ||  
+                mimetype === 'application/excel' || 
+                mimetype === 'application/vnd.ms-excel' || 
+                mimetype === 'application/vnd.msexcel' || 
+                mimetype === 'text/anytext' ||
+                mimetype === 'text/comma-separated-values' ||(mimetype === 'application/octet-stream' && fileExtension === 'csv')){
+                //Mime type is of CSV type
+
+                //Path where file will be uploaded.
+                fstream = fs.createWriteStream('./public/uploaded/' + filename);
+                file.pipe(fstream);
+                fstream.on('close', function () {    
+                    console.log("Upload Finished of " + filename);              
+                    res.redirect('back');           //where to go next
+                });
+            }else{
+                //Not a CSV, error
+                res.send(500,"Server Error"); 
+                 return;
+            }
+            
+
+        });
+});
 module.exports = router;
--- web.js
+++ web.js
@@ -1,34 +1,35 @@
 // web.js
 var express = require('express');
-var debug = require('debug')('http');
-var logfmt = require("logfmt");
-//var db = require('./mysql.js');
 var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var busboy = require('connect-busboy');
+//...
+
 var bodyParser = require('body-parser');
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(busboy()); 
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
 
-/// catch 404 and forward to error handler
+/// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
@@ -58,18 +59,17 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
+
+
+module.exports = app;
+
 app.set('port', process.env.PORT || 3000);
 
 
 //SERVERS
 var server = app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + server.address().port);
-    debug('Express server listening on port ' + server.address().port);
-
 });
-//Uncomment to enable ssl
-// var httpsServer = https.createServer(config, app);
-// httpsServer.listen(8443);
 
 
 
