f10b3a80e8c18214a6ab733e9f6519338d7dec3b
Tomdanizer <tomdanizer@gmail.com>
2014-07-21 18:29:23 -0500

    removed uneccessary comment

--- public/js/upload.js
+++ public/js/upload.js
@@ -29,17 +29,6 @@ var upload = (function(){
                     //document.location.reload(true);
                 }
             });
-           // this.parentNode.submit();
-            // files is a FileList of File objects. List some properties.
-            /* var output = [];
-             for (var i = 0, f; f = files[i]; i++) {
-             output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-             f.size, ' bytes, last modified: ',
-             f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
-             '</li>');
-             }
-             document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
-             */
         };
 
     return{
